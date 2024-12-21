60f8e6b702d688044caa63126fadea578d87ef00
devops <devops@v2702532.hosted-by-vdsina.ru>
2024-12-21 11:22:54 +0300

    ELK

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import logging
+from logstash_formatter import LogstashFormatterV1
 from flask import Flask, jsonify
 from flask_cors import CORS
 from prometheus_flask_exporter import PrometheusMetrics
@@ -9,6 +11,13 @@ metrics.info('app_info', 'Application info', version='1.0.0')
 
 app = Flask(__name__)
 CORS(app)  # Включаем CORS для всех маршрутов
+
+logger = logging.getLogger("flask_app")
+logger.setLevel(logging.INFO)
+
+logstash_handler = logging.StreamHandler()
+logstash_handler.setFormatter(LogstashFormatterV1())
+logger.addHandler(logstash_handler)
 
 @app.route('/api/message')
 def hello_world():
